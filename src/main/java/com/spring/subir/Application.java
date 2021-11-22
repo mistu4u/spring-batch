9c4e442c82b25b51f65fbf0de16bc5c2e36d4ef6
adhikarisubir@gmail.com <74544776+subiradhikari@users.noreply.github.com>
2021-11-22 17:14:37 -0500

    Moved job runner configuration to another file, else scheduled annotation was not working

--- src/main/java/com/spring/subir/Application.java
+++ src/main/java/com/spring/subir/Application.java
@@ -1,49 +1,20 @@
 package com.spring.subir;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, BatchAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableScheduling
 @EnableBatchProcessing
 @Slf4j
 public class Application {
 
-    @Autowired
-    private JobLauncher jobLauncher;
-
-    @Autowired
-    private Job job;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-
-    @Bean
-    public JobExecution run() {
-        JobExecution jobExecution = null;
-        try {
-            JobParameters jobParameters = new JobParametersBuilder()
-                    .addLong("time", System.currentTimeMillis()).toJobParameters();
-
-            jobExecution = jobLauncher.run(job, jobParameters);
-            log.info("Exit Status : " + jobExecution.getStatus());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return jobExecution;
     }
 }
---
+++ src/main/java/com/spring/subir/runner/JobRunner.java
@@ -0,0 +1,42 @@
+package com.spring.subir.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JobRunner {
+    @Value("${spring.batch.job.enabled}")
+    private String isEnabled;
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
+
+    @Bean
+    @Scheduled(cron="0 13 17 * * ?")
+    public JobExecution run() {
+        JobExecution jobExecution = null;
+        try {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis()).toJobParameters();
+
+            jobExecution = jobLauncher.run(job, jobParameters);
+            log.info(isEnabled);
+            log.info("Exit Status : " + jobExecution.getStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jobExecution;
+    }
+}
